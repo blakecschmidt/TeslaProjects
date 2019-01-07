8d0381b905f1ccabe041015fa691facf7cd4835b
Blake Schmidt <blakecschmidt@gmail.com>
2019-01-06 20:40:57 -0600

    checking for invalid token when jobs begin

--- MorningWeatherCheck.py
+++ MorningWeatherCheck.py
@@ -1,13 +1,44 @@
 #!/usr/bin/env python3
 
 import requests
+import json
+import time
+from datetime import datetime
+from datetime import timedelta
+
 from Constants import base_uri
+from OauthTokenRefresh import refresh_oauth_token
 
 
-#this will return 404 when car is asleep
-#wake_up = requests.get(f"https://owner-api.teslamotors.com/api/1/vehicles/{id}/wake_up", headers=header)
+def morning_weather_check():
 
-#this only works when the car is awake (when I manually wake up my car via phone app or getting in it)
-#charge_state = requests.get(f"https://owner-api.teslamotors.com/api/1/vehicles/{id}/data_request/charge_state", headers=header)
-#print(charge_state.json())
+    with open("secrets.json") as secrets_file:
+        args = eval(secrets_file.read())
+        access_token, token_timestamp, id = args["access_token"], args["token_timestamp"], args["id"]
+
+    token_expiration_ts = datetime.strptime(token_timestamp, "%Y-%m-%d %H:%M:%S") + timedelta(days=45)
+    current_ts = datetime.utcnow()
+
+    if current_ts > token_expiration_ts:
+        refresh_oauth_token()
+        time.sleep(5)
+
+        with open("secrets.json") as secrets_file:
+            args = eval(secrets_file.read())
+            access_token, id = args["access_token"], args["id"]
+
+    header = {"user-agent": "morning-weather-check",
+              "Authorization": f"Bearer {access_token}",
+              "Content-Type": "application/json; charset=utf-8"}
+
+    #this will return 404 when car is asleep
+    wake_up = requests.get(f"https://owner-api.teslamotors.com/api/1/vehicles/{id}/wake_up", headers=header)
+    print(wake_up)
+
+    #this only works when the car is awake (when I manually wake up my car via phone app or getting in it)
+    #charge_state = requests.get(f"https://owner-api.teslamotors.com/api/1/vehicles/{id}/data_request/charge_state", headers=header)
+    #print(charge_state.json())
+
+    
+morning_weather_check()
 
--- OauthTokenRequest.py
+++ OauthTokenRequest.py
@@ -6,47 +6,52 @@ from datetime import datetime
 
 from Constants import base_uri
 
-with open("secrets.json") as secrets_file:
-    args = eval(secrets_file.read())
 
-oauth_token_request_data = {
-  "grant_type": "password",
-  "client_id": args["client_id"],
-  "client_secret": args["client_secret"],
-  "email": args["email"],
-  "password": args["password"]
-}
+def oauth_token_request():
 
-oauth_token = requests.post(base_uri + "oauth/token?grant_type=password",
-                            headers={"user-agent": "tesla_oauth_grant"}, data=oauth_token_request_data)
+    with open("secrets.json") as secrets_file:
+        args = eval(secrets_file.read())
 
-header = {"user-agent": "vehicles-request/0.0.1",
-           "Authorization": f"Bearer {oauth_token.json()['access_token']}",
-           "Content-Type": "application/json; charset=utf-8"}
+    oauth_token_request_data = {
+      "grant_type": "password",
+      "client_id": args["client_id"],
+      "client_secret": args["client_secret"],
+      "email": args["email"],
+      "password": args["password"]
+    }
 
-vehicles = requests.get("https://owner-api.teslamotors.com/api/1/vehicles", headers=header)
-vehicle_count = vehicles.json()["count"]
+    oauth_token = requests.post(base_uri + "oauth/token?grant_type=password",
+                                headers={"user-agent": "tesla_oauth_grant"}, data=oauth_token_request_data)
 
-if vehicle_count > 1:
-    print("You have more than one vehicle tied to your account:\n")
-    for idx in range(0, vehicle_count):
-        print(f"{idx + 1}. {vehicles.json()['response'][idx]['display_name']}")
-    vehicle_idx = input("\nEnter the number of the vehicle you would like to use: ")
-    id = vehicles.json()["response"][vehicle_idx]["id"]
+    header = {"user-agent": "vehicles-request",
+               "Authorization": f"Bearer {oauth_token.json()['access_token']}",
+               "Content-Type": "application/json; charset=utf-8"}
 
-elif vehicle_count < 1:
-    print("No vehicles were found for this account.")
-    id = None
-    exit()
+    vehicles = requests.get("https://owner-api.teslamotors.com/api/1/vehicles", headers=header)
+    vehicle_count = vehicles.json()["count"]
 
-else:
-    id = vehicles.json()["response"][0]["id"]
+    if vehicle_count > 1:
+        print("You have more than one vehicle tied to your account:\n")
+        for idx in range(0, vehicle_count):
+            print(f"{idx + 1}. {vehicles.json()['response'][idx]['display_name']}")
+        vehicle_idx = input("\nEnter the number of the vehicle you would like to use: ")
+        id = vehicles.json()["response"][vehicle_idx]["id"]
 
-args["access_token"] = oauth_token.json()["access_token"]
-args["token_timestamp"] = datetime.utcfromtimestamp(oauth_token.json()["created_at"])
-args["refresh_token"] = oauth_token.json()["refresh_token"]
-args["id"] = str(id)
+    elif vehicle_count < 1:
+        print("No vehicles were found for this account.")
+        id = None
+        exit()
+
+    else:
+        id = vehicles.json()["response"][0]["id"]
+
+    args["access_token"] = oauth_token.json()["access_token"]
+    args["token_timestamp"] = datetime.utcfromtimestamp(oauth_token.json()["created_at"])
+    args["refresh_token"] = oauth_token.json()["refresh_token"]
+    args["id"] = str(id)
+
+    with open("secrets.json", "w") as secrets_file:
+        json.dump(args, secrets_file, indent=2, default=str)
 
 
-with open("secrets.json", "w") as secrets_file:
-    json.dump(args, secrets_file, indent=2, default=str)
+oauth_token_request()
