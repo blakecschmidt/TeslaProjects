553d6a2c8afd79888f5a6895dea43af620d2d5f8
Blake Schmidt <blakecschmidt@gmail.com>
2019-01-06 18:02:05 -0600

    added token refresh job

---
+++ OauthTokenRefresh.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import requests
+import json
+from datetime import datetime
+
+from Constants import base_uri
+
+with open("secrets.json") as secrets_file:
+    args = eval(secrets_file.read())
+
+oauth_token_refresh_data = {
+  "grant_type": "refresh_token",
+  "client_id": args["client_id"],
+  "client_secret": args["client_secret"],
+  "refresh_token": args["refresh_token"]
+}
+
+oauth_token = requests.post(base_uri + "oauth/token?grant_type=refresh_token",
+                            headers={"user-agent": "tesla_oauth_refresh"}, data=oauth_token_refresh_data)
+
+args["access_token"] = oauth_token.json()["access_token"]
+args["token_timestamp"] = datetime.utcfromtimestamp(oauth_token.json()["created_at"])
+args["refresh_token"] = oauth_token.json()["refresh_token"]
+
+with open("secrets.json", "w") as secrets_file:
+    json.dump(args, secrets_file, indent=2, default=str)
--- OauthTokenRequest.py
+++ OauthTokenRequest.py
@@ -9,7 +9,7 @@ from Constants import base_uri
 with open("secrets.json") as secrets_file:
     args = eval(secrets_file.read())
 
-oauth_grant_request_data = {
+oauth_token_request_data = {
   "grant_type": "password",
   "client_id": args["client_id"],
   "client_secret": args["client_secret"],
@@ -17,11 +17,11 @@ oauth_grant_request_data = {
   "password": args["password"]
 }
 
-oauth_grant = requests.post(base_uri + "oauth/token?grant_type=password",
-                            headers={"user-agent": "tesla_oauth_grant"}, data=oauth_grant_request_data)
+oauth_token = requests.post(base_uri + "oauth/token?grant_type=password",
+                            headers={"user-agent": "tesla_oauth_grant"}, data=oauth_token_request_data)
 
 header = {"user-agent": "vehicles-request/0.0.1",
-           "Authorization": f"Bearer {oauth_grant.json()['access_token']}",
+           "Authorization": f"Bearer {oauth_token.json()['access_token']}",
            "Content-Type": "application/json; charset=utf-8"}
 
 vehicles = requests.get("https://owner-api.teslamotors.com/api/1/vehicles", headers=header)
@@ -42,9 +42,9 @@ elif vehicle_count < 1:
 else:
     id = vehicles.json()["response"][0]["id"]
 
-args["access_token"] = oauth_grant.json()["access_token"]
-args["token_timestamp"] = datetime.utcfromtimestamp(oauth_grant.json()["created_at"])
-args["refresh_token"] = oauth_grant.json()["refresh_token"]
+args["access_token"] = oauth_token.json()["access_token"]
+args["token_timestamp"] = datetime.utcfromtimestamp(oauth_token.json()["created_at"])
+args["refresh_token"] = oauth_token.json()["refresh_token"]
 args["id"] = str(id)
 
 
