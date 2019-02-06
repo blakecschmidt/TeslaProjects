2f02f382ac04b5921338f89f729fdad473f91df8
Blake Schmidt <blakecschmidt@gmail.com>
2019-02-05 23:13:06 -0600

    added seat heater and temp change functionality

--- Constants.py
+++ Constants.py
@@ -1,1 +1,3 @@
-base_uri = "https://owner-api.teslamotors.com/"
+base_uri = "https://owner-api.teslamotors.com"
+min_avail_temp = 15.0
+max_avail_temp = 28.0
--- MorningWeatherCheck.py
+++ MorningWeatherCheck.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import requests
-import json
 import time
 from datetime import datetime
 from datetime import timedelta
@@ -21,22 +20,63 @@ def morning_weather_check():
 
     if current_ts > token_expiration_ts:
         refresh_oauth_token()
-        time.sleep(5)
-
         with open("secrets.json") as secrets_file:
             args = eval(secrets_file.read())
             access_token, id = args["access_token"], args["id"]
 
     header = {"user-agent": "morning-weather-check",
-              "Authorization": f"Bearer {access_token}",
-              "Content-Type": "application/json; charset=utf-8"}
+              "Authorization": f"Bearer {access_token}"
+              }
 
-    #wake_up = requests.post(f"https://owner-api.teslamotors.com/api/1/vehicles/{id}/wake_up", headers=header)
-    #print(wake_up.json())
+    # Wake up vehicle and wait until its online
+    wake_up = requests.post(f"{base_uri}/api/1/vehicles/{id}/wake_up", headers=header)
+    time.sleep(60)
 
-    #this only works when the car is awake (when I manually wake up my car via phone app or getting in it)
-    #charge_state = requests.get(f"https://owner-api.teslamotors.com/api/1/vehicles/{id}/data_request/charge_state", headers=header)
-    #print(charge_state.json())
+    climate_state = requests.get(f"{base_uri}/api/1/vehicles/{id}/data_request/climate_state", headers=header)
+    print(climate_state.json())
+    inside_temp = climate_state.json()["response"]["inside_temp"]*(9/5) + 32
+    print(inside_temp)
+
+    seat_heater_driver_medium = {
+                                 "heater": "0",
+                                 "level": "2"
+                                 }
+    set_temps_72_f = {
+                      "driver_temp": "22.2",
+                      "passenger_temp": "22.2"
+                      }
+    set_temps_77_f = {
+                      "driver_temp": "25.0",
+                      "passenger_temp": "25.0"
+                      }
+    set_temps_80_f = {
+                      "driver_temp": "26.6",
+                      "passenger_temp": "26.6"
+                      }
+
+    if 50 <= inside_temp < 60:
+        requests.post(f"{base_uri}/api/1/vehicles/{id}/command/auto_conditioning_start",
+                      header=header)
+        requests.post(f"{base_uri}/api/1/vehicles/{id}/command/remote_seat_heater_request", headers=header,
+                      data=seat_heater_driver_medium)
+        requests.post(f"{base_uri}/api/1/vehicles/{id}/command/set_temps", headers=header,
+                      data=set_temps_72_f)
+
+    elif 40 <= inside_temp < 50:
+        requests.post(f"{base_uri}/api/1/vehicles/{id}/command/auto_conditioning_start",
+                      header=header)
+        requests.post(f"{base_uri}/api/1/vehicles/{id}/command/remote_seat_heater_request", headers=header,
+                      data=seat_heater_driver_medium)
+        requests.post(f"{base_uri}/api/1/vehicles/{id}/command/set_temps", headers=header,
+                      data=set_temps_77_f)
+
+    elif inside_temp < 40:
+        requests.post(f"{base_uri}/api/1/vehicles/{id}/command/auto_conditioning_start",
+                      header=header)
+        requests.post(f"{base_uri}/api/1/vehicles/{id}/command/remote_seat_heater_request", headers=header,
+                      data=seat_heater_driver_medium)
+        requests.post(f"{base_uri}/api/1/vehicles/{id}/command/set_temps", headers=header,
+                      data=set_temps_80_f)
 
 
 if __name__ == "__main__":
