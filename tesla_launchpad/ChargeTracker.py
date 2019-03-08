a6e4f6b0d65567cc150e25c39bbf2beb59d8457b
Blake Schmidt <blakecschmidt@gmail.com>
2019-03-07 23:16:38 -0600

    added austin energy price lookup

--- tesla_launchpad/ChargeTracker.py
+++ tesla_launchpad/ChargeTracker.py
@@ -5,6 +5,8 @@ import boto3
 import time
 import json
 import os
+import re
+from bs4 import BeautifulSoup
 
 from tesla_launchpad.Constants import base_uri, battery_capacity_kwh, fence_points
 from tesla_launchpad.OauthTokenRefresh import refresh_oauth_token
@@ -92,17 +94,62 @@ def charge_tracker():
     print(f"latitude: {latitude}")
     print(f"longitude: {longitude}")
     print(f"battery_kwh: {battery_level_kwh}")
-    # charging_state: Charging/ Disconnected/
+    # charging_state: Charging/ Disconnected/ Complete
+    #"charge_port_latch": "Engaged"
+    #"charging_state": "Complete"
     fence = Fence(fence_points)
     if fence.check_point(current_location):
-        isHome = True
+        is_home = True
         print("Vehicle is inside the geofence.")
     else:
-        isHome = False
+        is_home = False
         print("Vehicle is outside the geofence.")
 
+    if not is_home:
+        return
+
+    ae_prices_url = "https://austinenergy.com/ae/rates/residential-rates/residential-electric-rates-and-line-items"
+    r = requests.get(ae_prices_url, headers={
+        'User-Agent':
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
+    })
+    r.raise_for_status()
+
+    soup = BeautifulSoup(r.text, 'html.parser')
+    ae_data_table = soup.find_all(class_="aeDataTable")[0]
+    table_rows = [str(row) for row in BeautifulSoup(ae_data_table.prettify(), "html5lib").select("tr")]
+    #print(BeautifulSoup(ae_data_table.prettify(), "html5lib").select("tr"))
+
+    tier_regex = re.compile(r'Tier 2:')
+    tier_filter = filter(tier_regex.search, table_rows)
+    price_regex = re.compile(r'\d*\.\d*¢')
+    tier2_rate = float(price_regex.search(list(tier_filter)[0]).group().replace("¢", ""))
+
+    adjustment_regex = re.compile(r'Power Supply Adjustment')
+    adjustment_filter = filter(adjustment_regex.search, table_rows)
+    adjustment_rate = float(price_regex.search(list(adjustment_filter)[0]).group().replace("¢", ""))
+
+    assistance_regex = re.compile(r'Customer Assistance Program')
+    assistance_filter = filter(assistance_regex.search, table_rows)
+    assistance_rate = float(price_regex.search(list(assistance_filter)[0]).group().replace("¢", ""))
+
+    street_regex = re.compile(r'Service Area Street Lighting')
+    street_filter = filter(street_regex.search, table_rows)
+    street_rate = float(price_regex.search(list(street_filter)[0]).group().replace("¢", ""))
+
+    efficiency_regex = re.compile(r'Energy Efficiency Programs')
+    efficiency_filter = filter(efficiency_regex.search, table_rows)
+    efficiency_rate = float(price_regex.search(list(efficiency_filter)[0]).group().replace("¢", ""))
+
+    regulatory_regex = re.compile(r'Regulatory Charge')
+    regulatory_filter = filter(regulatory_regex.search, table_rows)
+    regulatory_rate = float(price_regex.search(list(regulatory_filter)[0]).group().replace("¢", ""))
+
+    price_per_kwh = tier2_rate + adjustment_rate + assistance_rate + street_rate + efficiency_rate + regulatory_rate
+    print(price_per_kwh)
+
+
     '''
-    get rates from austin energy site https://austinenergy.com/ae/rates/residential-rates/residential-electric-rates-and-line-items
     set up amazon iot button
     get pandas for csv creation (or create an aws database)
     schema = start_ts|start_kwh|stop_ts|stop_kwh|total_charge_kwh
